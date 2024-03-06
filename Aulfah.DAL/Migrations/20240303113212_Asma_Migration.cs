d6f1ce998a78f5c9aeb18fc4e80ca8fa07876778
AsmaAlharrasi <asmaalharrasi18@gmail.com>
2024-03-06 09:55:44 +0400

    pull master by Asma

---
+++ Aulfah.DAL/Migrations/20240303113212_Asma_Migration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aulfah.DAL.Migrations
+{
+    public partial class Asma_Migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
